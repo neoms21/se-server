f0506d5b0a38d32c097f88fb1d6e9b1b1df21c21
Mark Shurmer <mark.shurmer@interactconsultants.co.uk>
2017-01-19 17:16:43 +0000

    add socket io auth beginning

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
 'use strict';
-var logger = require('bunyan');
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+const logger = require('bunyan');
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
-var mongoRepository = require('./db/mongo-repository');
-var commandMediator = require('./cqrs/command-mediator');
-var eventMediator = require('./cqrs/event-mediator');
-var deNormalizerManager = require('./cqrs/denormalizer-mediator');
+const mongoRepository = require('./db/mongo-repository');
+const commandMediator = require('./cqrs/command-mediator');
+const eventMediator = require('./cqrs/event-mediator');
+const deNormalizerManager = require('./cqrs/denormalizer-mediator');
+const signingRoutes = require('./routes/signing-routes');
+const socketHandler = require('./socket-handler');
 
 // create our logger
 var log = logger.createLogger({
@@ -30,16 +32,8 @@ mongoRepository.createOrOpenDb();
 //     res.sendfile('index.html');
 // });
 
-io.on('connection', function (socket) {
-    log.info('a user connected');
-
-    socket.on('command', function(cmdReq) {
-        log.info('command received: ' + JSON.stringify(cmdReq));
-        var cmd = commandMediator.createCommand(cmdReq);
-        commandMediator.dispatch(cmd);
-    });
-
-});
+// sockets related
+socketHandler(io, log);
 
 // send out events
 eventMediator.propagator.subscribe(function(ev) {
@@ -49,35 +43,11 @@ eventMediator.propagator.subscribe(function(ev) {
 // load any denormalizers
 deNormalizerManager.init(log);
 
+
+// any express routes
+signingRoutes(app, log);
+
 http.listen(8180, function () {
     console.log('listening on *:8180');
 });
-
-
-// log requests
-// server.pre(function (request, response, next) {
-//     log.info({req: JSON.stringify(request.body)}, 'REQUEST ' + JSON.stringify(request.body));
-//     // add correlation id
-//     response.header('correlation', uuid.v4());
-//     next();
-// });
-//
-// // socket io
-// io.sockets.on('connection', function (socket) {
-//     log.info('connected socket');
-//
-//     socket.emit('news', {hello: 'world'});
-//     socket.on('command', function (command:any) {
-//         console.log(command);
-//     });
-// });
-
-
-// add routes
-// commandRoutes(server);
-
-// server.listen(8180, function () {
-//     console.log('%s listening at %s', server.name, server.url);
-// });
-
 
--- src/socket-handler.js
+++ src/socket-handler.js
@@ -1,1 +1,26 @@
+'use strict';
+const commandMediator = require('./cqrs/command-mediator');
+const socketioAuth = require('socketio-auth');
+const mongoRepository = require('./db/mongo-repository');
 
+const processSockets = (io, log) => {
+    socketioAuth(io, {
+        authenticate: (socket, data, callback) => {
+            mongoRepository.getCount('logins', {email: data.email, password: data.password });
+        
+        }
+    });
+
+    io.on('connection', function (socket) {
+        log.info('a user connected');
+
+        socket.on('command', function (cmdReq) {
+            log.info('command received: ' + JSON.stringify(cmdReq));
+            const cmd = commandMediator.createCommand(cmdReq);
+            commandMediator.dispatch(cmd);
+        });
+
+    });
+};
+
+module.exports = processSockets;
