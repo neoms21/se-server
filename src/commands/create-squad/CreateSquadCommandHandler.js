04c224dd932ec86a77a839828f89cf29b106cd0d
neoms21 <neoms21@gmail.com>
2017-04-03 06:01:54 +0100

    changed response.next to arrays. not sure if it's correct or not. to be discussed

--- src/commands/create-squad/CreateSquadCommandHandler.js
+++ src/commands/create-squad/CreateSquadCommandHandler.js
@@ -19,46 +19,34 @@ function execute() {
 
 function verify() {
 
-    return Rx.Observable.of([]);
-    // var response = new Rx.Subject();
-    //
-    // setTimeout(function (command) { // use timeout as rx is async
-    //     console.log(command);
-    //     if (util.isNullOrUndefined(command.name)) {
-    //         response.next({name: 'Name property was not defined'});
-    //     }
-    //
-    //     if (util.isNullOrUndefined(command.email)) {
-    //         response.next({email: 'Email property was not defined'});
-    //     }
-    //
-    //     if (util.isNullOrUndefined(command.password)) {
-    //         response.next({password: 'Password property was not defined'});
-    //     } else {
-    //         if (command.password.length < 8) {
-    //             response.next({password: 'Password must be at least 8 characters long'});
-    //         }
-    //     }
-    //
-    //     const userNameToSearchFor = command.email || '';
-    //
-    //     // check that the user is not sending a duplicate
-    //     MongoRepository.getCount('logins', {userName: userNameToSearchFor})
-    //         .subscribe(function (count) {
-    //
-    //             if (count > 0) {
-    //                 // oops duplicate
-    //                 response.next({'email': 'The email ' + userNameToSearchFor + ' is a duplicate'});
-    //             }
-    //
-    //             // we are done
-    //             response.complete();
-    //         }, function (err) {
-    //             response.error(err);
-    //         });
-    // }, 100, {});
-    //
-    // return response;
+
+    let response = new Rx.Subject();
+    let errors = [];
+    setTimeout(function (command) { // use timeout as rx is async
+        console.log(command);
+        if (util.isNullOrUndefined(command.payload.squadName)) {
+            errors.push({squadName: 'Squad Name is mandatory'});
+        }
+
+        const squadNameToSearchFor = command.payload.squadName || '';
+
+        // check that the user is not sending a duplicate
+        MongoRepository.getCount('squads', {name: squadNameToSearchFor})
+            .subscribe(function (count) {
+
+                if (count > 0) {
+                    // oops duplicate
+                    errors.push({'squadName': 'The squad ' + squadNameToSearchFor + ' is a duplicate'});
+                }
+
+                // we are done
+                response.next(errors);
+            }, function (err) {
+                response.error(err);
+            });
+    }, 100, this.command);
+
+    return response;
 };
 
 module.exports = {
