ad98bb3cb21ad31276affe4c35a7b043614ec7e2
Mark Shurmer <mark.shurmer@interactconsultants.co.uk>
2017-06-14 17:42:33 +0100

    fix login denorm tests

--- src/denormalizers/logins-denormalizers-tests.js
+++ src/denormalizers/logins-denormalizers-tests.js
@@ -7,54 +7,54 @@ const eventMediator = require('../cqrs/event-mediator');
 const generalServices = require('../cqrs/general-services');
 
 describe('Login denormalizers', function () {
-  let countStub;
-  let count = 0;
-  let timeStub;
-  let loggerStub;
-  let dispatchStub;
+    let countStub;
+    let count = 0;
+    let timeStub;
+    let loggerStub;
+    let dispatchStub;
 
-  beforeEach(function () {
-    countStub = sinon.stub(mongoRepository, 'getCount', function () {
-      // supply dummy observable
-      return Rx.Observable.from([count]);
-    });
-    timeStub = sinon.stub(generalServices, 'getTime', () => new Date('01 Sep 2016 08:00'));
-    loggerStub = {
-      info: function () {
-      }
-    };
-    eventMediator.init(loggerStub);
-    dispatchStub = sinon.stub(eventMediator, 'dispatch', function () {
-    });
+    beforeEach(function () {
+        countStub = sinon.stub(mongoRepository, 'getCount', function () {
+            // supply dummy observable
+            return Rx.Observable.from([count]);
+        });
+        timeStub = sinon.stub(generalServices, 'getTime', () => new Date('01 Sep 2016 08:00'));
+        loggerStub = {
+            info: function () {
+            }
+        };
+        eventMediator.init(loggerStub);
+        dispatchStub = sinon.stub(eventMediator, 'dispatch', function () {
+        });
 
-  });
-
-  afterEach(function () {
-    countStub.restore();
-    timeStub.restore();
-    dispatchStub.restore();
-  });
-
-  describe('handleRegisterUser', function () {
-
-    it('should check the count', function (done) {
-      deNormalizer.handleMessage({id: 1});
-      chai.assert.isTrue(countStub.called);
-      done();
     });
 
-
-  });
-
-  describe('getMessages', function () {
-
-    it('should return correct messages', function () {
-
-      const msgList = deNormalizer.getMessages();
-      chai.assert.isArray(msgList, 'Should be array');
-      chai.assert.equal(msgList.length, 1);
-      chai.assert.equal(msgList[0], 'UserRegisteredEvent');
+    afterEach(function () {
+        countStub.restore();
+        timeStub.restore();
+        dispatchStub.restore();
     });
 
-  });
+    describe('handleRegisterUser', function () {
+
+        it('should check the count', function (done) {
+            deNormalizer.handleMessage({id: 1, command: {payload: {}}});
+            chai.assert.isTrue(countStub.called);
+            done();
+        });
+
+
+    });
+
+    describe('getMessages', function () {
+
+        it('should return correct messages', function () {
+
+            const msgList = deNormalizer.getMessages();
+            chai.assert.isArray(msgList, 'Should be array');
+            chai.assert.equal(msgList.length, 1);
+            chai.assert.equal(msgList[0], 'UserRegisteredEvent');
+        });
+
+    });
 });
