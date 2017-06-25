79e7d375b1d4b4a174e0624c7a91f9ac61c8be6d
Mark Shurmer <mark.shurmer@metrobank.plc.uk>
2017-06-25 14:46:22 +0100

    merged from master

--- src/queries/fetch-players-query.js
+++ src/queries/fetch-players-query.js
@@ -6,34 +6,33 @@ const ObjectId = require('mongodb').ObjectId;
 const logger = require('../core/logger').logger();
 const EventFactory = require('./../cqrs/event-factory');
 const execute = (query) => {
-    console.log(query);
-    const ret = new Rx.Subject();
-    let items = [];
-    MongoRepository.query('squads', {_id: ObjectId(query.payload.id)}, {players: 1})
-        .subscribe(function (x) {
-            items.push(x);
-        }, function (err) {
-            logger.error(err);
-            ret.error(err);
-        }, function () {
-            logger.info(`Players found for squad id ${query.payload.id} ${items}`);
-            let event = EventFactory.createFromQuery(query, 'FetchPlayersEvent', false);
-            event.messageNumber = 1;
-            event.maxMessages = 1;
-            event.data = items[0].players;
-            ret.next(event);
-        });
-    return ret;
+  console.log(query);
+  const ret = new Rx.Subject();
+  let items = [];
+  MongoRepository.query('squads', {_id: ObjectId(query.payload.id)}, {players: 1})
+    .subscribe(function (x) {
+      items.push(x);
+    }, function (err) {
+      logger.error(err);
+      ret.error(err);
+    }, function () {
+      logger.info(`Players found for squad id ${query.payload.id} ${items}`);
+      let event = EventFactory.createFromQuery(query, 'FetchPlayersEvent', false);
+      event.messageNumber = 1;
+      event.maxMessages = 1;
+      event.data = items[0].players;
+      ret.next(event);
+    });
+  return ret;
 };
 
 const verify = () => {
-    // send back empty
-    return Rx.Observable.empty();
+  // send back empty
+  return Rx.Observable.empty();
 };
 
 module.exports = {
-    verify: verify,
-    execute: execute,
-    getQuery: () => "FetchPlayers"
+  verify: verify,
+  execute: execute,
+  getQuery: () => "FetchPlayers"
 };
-
